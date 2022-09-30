ccbe67dc4ccab2a1c43273e4ebba6be73deff374
José Luis Acosta <jluis.acosta@outlook.com>
2022-09-29 21:43:50 -0500

    Update site global title

--- components/_App/Layout.js
+++ components/_App/Layout.js
@@ -20,7 +20,7 @@ const Layout = ({ children }) => {
                     name="viewport" 
                     content="width=device-width, initial-scale=1, shrink-to-fit=no" 
                 />
-                <title>Reton - Resume CV React Next.js Template</title>
+                <title>José Luis Acosta - Software Engineer</title>
             </Head>
 
             {children}
