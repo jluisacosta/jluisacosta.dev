2f5cc3883212e9a15c53faaf963c912e0b91f950
José Luis Acosta <jluis.acosta@outlook.com>
2022-09-29 22:25:04 -0500

    Implement GitHub Actions Deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const path = require('path')
+const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
+    assetPrefix: isProd ? '/jluisacosta/' : '',
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
     },
