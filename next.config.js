6b14131eeaf7c84bf33600228dd4cdcd643a5e24
José Luis Acosta <jluis.acosta@outlook.com>
2022-09-29 22:35:27 -0500

    Repo name as asset prefix

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-    assetPrefix: isProd ? '/jluisacosta/' : '',
+    assetPrefix: isProd ? '/jluisacosta.dev/' : '',
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
     },
