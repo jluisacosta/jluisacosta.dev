fcc2c3f241893c03fca20afab9d260fa99c38bd7
José Luis Acosta <jluis.acosta@outlook.com>
2022-09-29 22:51:02 -0500

    Remove asset prefix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
-const path = require('path')
-const isProd = process.env.NODE_ENV === 'production';
+const path = require('path');
 
 module.exports = {
-    assetPrefix: isProd ? '/jluisacosta.dev/' : '',
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
     },
